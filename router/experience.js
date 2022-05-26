555633dbb336ac114c160c0f17c623715ff64b4b
naderelfkey <73117120+naderelfeky@users.noreply.github.com>
2022-05-26 04:45:12 +0200

    experence

--- router/experience.js
+++ router/experience.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const experencecontroller = require('../controller/experencecontroller');
+  
 
 router.post('/newexperence', experencecontroller.create);
 router.get('/allexperence', experencecontroller.getAll);
